a5d830fab55f81603d8c52b8b081cdbd85c6b790
ahmetson <ahmetson@zoho.com>
2022-03-02 00:28:54 +0400

    remove single token defi

--- migrations/3_game_singleToken_migration.js
+++
@@ -1,7 +0,0 @@
-var singleTokenStaking = artifacts.require("./mscpGame/ganeralDefi/SingleTokenStaking.sol");
-
-module.exports = function(deployer, _network) {
-	deployer.deploy(singleTokenStaking).then(function(){
-		console.log("single token staking on address: " + singleTokenStaking.address);
-	});
-};

67d85caf5c0cc27f63c99a947b05309256d28602
huabro <huabro@seascape.network>
2022-03-30 11:18:08 +0800

    beta support session

--- contracts/beta/MoonscapeBeta.sol
+++ contracts/beta/MoonscapeBeta.sol
@@ -1,53 +1,69 @@
 pragma solidity 0.6.7;
 
+import "./../openzeppelin/contracts/access/Ownable.sol";
 import "./../openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./../openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 
 /// @author Nejc Schneider
 /// Users lock 100k mscp during session, and can unlock it after its finished
-contract MoonscapeBeta {
+contract MoonscapeBeta is Ownable{
     using SafeERC20 for IERC20;
 
-    IERC20 public immutable token;
-    uint constant private requiredAmount = 300000 * 10**18;
-    uint public startTime;
-    uint public endTime;
+    uint public sessionId;
 
-    mapping(address => bool) public stakers;
+    struct Session {
+        uint startTime; // session start time
+        uint endTime;   // session end time
+        uint requiredAmount;    // unlock game need token amonut
+        IERC20 token;   //token address
+    }
+
+    mapping(uint => mapping(address => bool)) public stakers;
+    mapping(uint => Session) public sessions;
 
     event Transfer(address sender, bool locked);
+    event StartSession(uint sessionId, IERC20 token,uint requiredAmount, uint startTime, uint endTime);
 
-    constructor(IERC20 _token, uint _startTime, uint _endTime) public {
+    constructor() public {}
+
+    function startSession(IERC20 _token,uint _requiredAmount, uint _startTime, uint _endTime) external onlyOwner{
         require(address(_token) != address(0), "invalid currency address");
         require(_startTime > now, "session should start in future");
         require(_endTime > _startTime, "start time should precede end");
+        require(_requiredAmount > 0, "token number wrong");
 
-        token = _token;
-        startTime = _startTime;
-        endTime = _endTime;
+        sessionId++;
+
+        sessions[sessionId] = Session(_startTime, _endTime, _requiredAmount, _token);
+
+        emit StartSession(sessionId, _token, _requiredAmount, _startTime, _endTime);
     }
 
-    function lock() external {
-        require(startTime < now, "session hasnt started yet");
-        require(endTime > now, "session is finished");
-        require(!stakers[msg.sender], "tokens already locked");
-        require(token.balanceOf(msg.sender) >= requiredAmount, "insufficient mscp user balance");
+    function lock(uint _sessionId) external {
+        Session storage session = sessions[_sessionId];
+        require(session.startTime < now, "session hasnt started yet");
+        require(session.endTime > now, "session is finished");
+        require(!stakers[_sessionId][msg.sender], "tokens already locked");
+        require(session.requiredAmount > 0, "token number wrong");
+        require(session.token.balanceOf(msg.sender) >= session.requiredAmount, "insufficient mscp user balance");
 
-        stakers[msg.sender] = true;
+        stakers[_sessionId][msg.sender] = true;
 
-        token.safeTransferFrom(msg.sender, address(this), requiredAmount);
+        session.token.safeTransferFrom(msg.sender, address(this), session.requiredAmount);
 
         emit Transfer(msg.sender, true);
     }
 
-    function unlock() external{
-        require(now > endTime);
-        require(stakers[msg.sender], "no tokens locked");
+    function unlock(uint _sessionId) external{
+        Session storage session = sessions[_sessionId];
+        require(now > session.endTime);
+        require(stakers[_sessionId][msg.sender], "no tokens locked");
+        require(session.requiredAmount > 0, "token number wrong");
 
-        stakers[msg.sender] = false;
+        stakers[_sessionId][msg.sender] = false;
 
-        token.safeTransfer(msg.sender, requiredAmount);
+        session.token.safeTransfer(msg.sender, session.requiredAmount);
 
         emit Transfer(msg.sender, false);
     }
